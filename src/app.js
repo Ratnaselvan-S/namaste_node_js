ddaedb3fa70e3bb204fdc0533fc476e6637fa14d
Ratna selvan S <selvanratna2004@gmail.com>
2026-02-17 16:13:45 +0530

    episode 18 completed

--- src/app.js
+++ src/app.js
@@ -2,8 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("server tested");
+// app.use("/", (req, res) => {
+//   res.send("server noted");
+// });
+// app.use("/test", (req, res) => {
+//   res.send("server tested");
+// });
+
+// app.use("/test/2", (req, res) => {
+//   res.send("server 2 tested");
+// });
+
+app.get("/users", (req, res) => {
+  res.send({ firstname: "something" });
 });
 app.listen(7777, () => {
   console.log("Server started");
