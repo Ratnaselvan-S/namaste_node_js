e5ed4d72f6566e65f365f605685fd91a34eabe84
Ratna selvan S <selvanratna2004@gmail.com>
2026-02-19 19:20:49 +0530

    episode 21  completed

--- src/app.js
+++ src/app.js
@@ -5,13 +5,10 @@ const app = express();
 const connectDb = require("./config/database");
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Asksay saini",
-    lastName: "Something",
-    emailId: "soething@email",
-    password: "something",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
